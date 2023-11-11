90e12d41afbb6ad447d399f715cf2916ffefb1a5
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-11 14:47:46 +0700

    feat: add Padding component

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,13 +1,14 @@
 import Bg from "@/assets/header/orangeBg.png";
 import Image from "next/image";
+import Padding from "../padding/padding";
 
 export default function Header() {
   return (
     <div className="relative">
       <Image className="absolute z-[-2]" src={Bg} alt="" />
-      <div className={``}>
-        <div>kkkkk</div>
-      </div>
+      <Padding>
+        <div className="pt-[11rem]">kkkkk</div>
+      </Padding>
     </div>
   );
 }
---
+++ app/components/padding/padding.tsx
@@ -0,0 +1,3 @@
+export default function Padding({ children }: { children: React.ReactNode }) {
+  return <div className="w-full px-[3rem]">{children}</div>;
+}
