4e6a0758594ca31f322e19c9e3799fc20e6a2878
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-12 19:17:48 +0700

    refacor: resize px in mobile size

--- app/components/padding/padding.tsx
+++ app/components/padding/padding.tsx
@@ -1,3 +1,3 @@
 export default function Padding({ children }: { children: React.ReactNode }) {
-  return <div className='w-full px-[3rem]'>{children}</div>;
+  return <div className='w-full px-[1.4rem] md:px-[3rem]'>{children}</div>;
 }
