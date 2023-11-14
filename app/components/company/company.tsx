d980736644106dce161f4e88fe009a79bc2b0059
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-14 17:01:41 +0700

    fix: fix size company logo & title text

--- app/components/company/company.tsx
+++ app/components/company/company.tsx
@@ -1,28 +1,21 @@
-import { useState } from 'react';
 import { CompanyIcon } from './companyData';
 import styles from './style.module.css';
 import Image from 'next/image';
 import Padding from '../padding/padding';
 
 export default function Company() {
-  const [isHover, setIsHover] = useState(false);
-
-  const handleHover = () => {
-    setIsHover(!isHover);
-  };
-
   return (
     <Padding>
       <div className='w-full flex  flex-col justify-center items-center mt-[6rem] mb-[3rem]'>
         <div>
           <h1
-            className='text-[1.6rem] text-[#696984s] text-center font-[500]'
+            className='text-[1.2rem] md:text-[1.6rem] text-[#696984s] text-center font-[500]'
             style={{ fontFamily: 'poppins, sans-serif' }}
           >
             Trusted by 5,000+ Companies Worldwide
           </h1>
         </div>
-        <div className='flex items-center flex-wrap justify-center'>
+        <div className='mt-[2rem] flex items-center flex-wrap justify-center'>
           {CompanyIcon.map((item, index) => (
             <div className={styles.imgContainer} key={index}>
               <Image className={styles.imgRegular} src={item.regular} alt='' />
