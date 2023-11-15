5b279050caff97ff40baa6967efeda4e55a9e380
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-15 18:45:13 +0700

    feat: add assets starter pack

--- app/components/features/features.tsx
+++ app/components/features/features.tsx
@@ -4,7 +4,24 @@ export default function Features() {
   return (
     <Padding>
       <div>
-        <h1>Halo</h1>
+        <div>
+          <div className='flex flex-col items-center'>
+            <h1
+              className='text-[#2F327D] text-[1.5rem] font-[600] text-center'
+              style={{ fontFamily: 'poppins, sans-serif' }}
+            >
+              Our <span className='text-[#F48C06]'>Features</span>
+            </h1>
+            <p
+              className='text-[#696984] text-center mt-[1rem]'
+              style={{ fontFamily: 'poppins' }}
+            >
+              This very extraordinary feature, can make learning activities more
+              efficient
+            </p>
+          </div>
+          <div></div>
+        </div>
       </div>
     </Padding>
   );
