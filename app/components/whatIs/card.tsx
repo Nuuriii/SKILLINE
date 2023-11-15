8d428bc6b0ee2a4ac291822d23333eca207d1940
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-15 12:41:32 +0700

    feat: add hover effect in desktop view

--- app/components/whatIs/card.tsx
+++ app/components/whatIs/card.tsx
@@ -10,20 +10,24 @@ interface CardProps {
 export default function Card({ index, bgImg, title, btnText }: CardProps) {
   return (
     <div
-      className={`relative flex h-[15rem] w-[20rem] items-center justify-center mb-[1rem] sm:mr-[2rem] bg-women`}
+      className={`relative flex xl:bg-[#171B4180] h-[20rem] rounded-[1rem] overflow-hidden w-[29rem] items-center justify-center mb-[1rem] sm:mr-[2rem] bg-women`}
     >
-      <div className='absolute h-[100%] w-[100%] z-[-1]'>
+      <div className='absolute  hidden xl:block h-[100%] w-[100%] z-[-1]'>
         <Image className='h-[100%] w-[100%]' src={bgImg} alt='' />
       </div>
       <div className='flex flex-col items-center justify-center'>
         <h1
-          className='text-[#2F327D] lg:text-white mb-[0.2rem] font-[600]'
+          className='text-[#2F327D] xl:text-[1.2rem] lg:text-white mb-[0.2rem] font-[600]'
           style={{ fontFamily: 'poppins, sans-serif' }}
         >
           {title}
         </h1>
         <button
-          className='h-[3rem] w-[12rem] rounded-[2rem] flex justify-center items-center bg-[#23BDEEE5] text-[#FFFFFF] font-[500] hover:scale-[1.03] duration-[0.3s] active:scale-[0.99] hover:bg-[#4acff7]'
+          className={`h-[3rem] w-[12rem] rounded-[2rem] flex justify-center items-center bg-[#23BDEEE5] text-[#FFFFFF] font-[500] hover:scale-[1.03] duration-[0.3s] active:scale-[0.99] hover:bg-[#4acff7] xl:text-[1rem] ${
+            index === 0
+              ? 'xl:bg-transparent xl:border-[1px] border-solid border-[#fff] xl:hover:border-none'
+              : ''
+          }`}
           style={{ fontFamily: 'poppins, sans-serif' }}
         >
           {btnText}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,13 +6,7 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        women: "url('/assets/whatIs/women.png')",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
 export default config;
