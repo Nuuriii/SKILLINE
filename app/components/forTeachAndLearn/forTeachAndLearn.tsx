9ce73fcaadd1a55f56a4400ceb2deaad01b989ac
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-16 17:33:30 +0700

    feat: add responsive design for tablet & dekstop

--- app/components/forTeachAndLearn/forTeachAndLearn.tsx
+++ app/components/forTeachAndLearn/forTeachAndLearn.tsx
@@ -5,18 +5,18 @@ import Image from 'next/image';
 export default function ForTeachAndLearn() {
   return (
     <Padding>
-      <div className='flex justify-center'>
-        <div className='min-[500px]:w-[30rem] xl:w-[60rem] lg:w-[100%]'>
-          <div className='flex flex-col items-center'>
+      <div className='mt-[1rem] lg:mt-[8rem] flex justify-center'>
+        <div className='flex flex-col lg:flex-row items-center min-[500px]:w-[30rem]  lg:w-[100%]'>
+          <div className='flex flex-col items-center lg:items-start lg:mr-[6rem] lg:w-[50%]'>
             <h1
-              className='text-[#2F327D] font-[600] text-center text-[1.5rem] w-[16rem]'
+              className='text-[#2F327D] font-[600] text-center lg:text-left text-[1.5rem] lg:text-[2rem] w-[16rem] lg:w-[20rem]'
               style={{ fontFamily: 'poppins, sans-serif' }}
             >
               <span className='text-[#F48C06]'>Tools</span> For Teachers And
               Learners
             </h1>
             <p
-              className='text-[#696984] text-center mt-[1rem]'
+              className='text-[#696984] text-center lg:text-justify lg:text-[1.2rem] mt-[1rem]'
               style={{ fontFamily: 'poppins,sans-serif' }}
             >
               Class has a dynamic set of teaching tools built to be deployed and
@@ -24,8 +24,12 @@ export default function ForTeachAndLearn() {
               for students to complete and submit.
             </p>
           </div>
-          <div className='mt-[1rem] flex justify-center'>
-            <Image className='h-[15rem] w-auto' src={Student} alt='' />
+          <div className='mt-[1rem] lg:w-[50%] flex justify-center'>
+            <Image
+              className='h-[15rem] lg:h-[20rem] xl:h-[22rem] w-auto'
+              src={Student}
+              alt=''
+            />
           </div>
         </div>
       </div>
