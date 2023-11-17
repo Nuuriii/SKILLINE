aee0b2d149ec8bc014c13384f6daa76edd849580
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-17 17:26:32 +0700

    feat: add responsive design for dekstop view

--- app/components/assesment/assesment.tsx
+++ app/components/assesment/assesment.tsx
@@ -6,21 +6,21 @@ import Plane from '@/assets/assesment/plane.svg';
 export default function Assesment() {
   return (
     <Padding>
-      <div className='mt-[6rem]'>
-        <div>
-          <div className='flex flex-col items-center'>
+      <div className='mt-[6rem] flex justify-center'>
+        <div className='min-[500px]:w-[30rem] min-[1444px]:w-[70rem] lg:w-[100%] lg:flex lg:justify-between '>
+          <div className='flex flex-col items-center xl:w-[50%]'>
             <div
-              className='h-[19rem] w-[16rem] p-[1rem] shadow-[0px_9px_43px_0px_#282b6f1a] relative rounded-[1rem]'
+              className='h-[19rem] xl:h-[24rem] w-[16rem] xl:w-[20rem] p-[1rem] shadow-[0px_9px_43px_0px_#282b6f1a] relative rounded-[1rem]'
               style={{ fontFamily: 'poppins, sans-serif' }}
             >
               <h3 className='h-[2rem] w-[6.5rem] flex justify-center items-center rounded-[2rem] bg-[#CDD8FF] text-[0.8rem] text-[#717FB0]'>
                 Question 1
               </h3>
-              <p className='text-[1.2rem] text-[#55578D] font-[600] mt-[1rem]'>
+              <p className='text-[1.2rem] text-[#55578D] font-[600] mt-[1rem] xl:mt-[1.5rem]'>
                 True or false? This play takes place in Italy
               </p>
               <Image
-                className='mt-[1.4rem] z-[1] absolute rounded-[1rem]'
+                className='mt-[1.4rem] z-[1] xl:h-[11rem] xl:w-auto absolute rounded-[1rem]'
                 src={River}
                 alt=''
               />
@@ -37,14 +37,14 @@ export default function Assesment() {
             </div>
           </div>
           <div
-            className='mt-[2rem] flex flex-col items-center'
+            className='mt-[2rem] flex flex-col items-center lg:items-start lg:w-[50%]'
             style={{ fontFamily: 'poppins, sans-serif' }}
           >
-            <h1 className='text-[#2F327D] font-[600] text-[1.2rem]'>
+            <h1 className='text-[#2F327D] font-[600] text-[1.5rem] lg:w-[25rem] lg:text-left lg:text-[2rem]'>
               Assessments, <span className='text-[#F48C06]'>Quizzes</span>,
               Tests
             </h1>
-            <p className='mt-[0.8rem] text-[#696984] text-center'>
+            <p className='mt-[0.8rem] text-[#696984] text-center lg:text-left lg:w-[30rem] lg:text-[1.2rem]'>
               Easily launch live assignments, quizzes, and tests. Student
               results are automatically entered in the online gradebook.
             </p>
