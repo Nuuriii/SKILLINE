8e7639d9d8a9ed7b33c1e71d95fbfd73a484642e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-17 23:15:16 +0700

    feat: add small bg

--- app/components/assesment/assesment.tsx
+++ app/components/assesment/assesment.tsx
@@ -2,37 +2,66 @@ import Padding from '../padding/padding';
 import Image from 'next/image';
 import River from '@/assets/assesment/river.png';
 import Plane from '@/assets/assesment/plane.svg';
+import Close from '@/assets/assesment/close-icon.png';
+import Success from '@/assets/assesment/success.png';
+import Purple from '@/assets/features/purpleBall.png';
+import GreenTransparent from '@/assets/assesment/green-transparent.svg';
 
 export default function Assesment() {
   return (
     <Padding>
-      <div className='mt-[6rem] flex justify-center'>
+      <div className='mt-[6rem] mb-[3rem] flex justify-center'>
         <div className='min-[500px]:w-[30rem] min-[1444px]:w-[70rem] lg:w-[100%] lg:flex lg:justify-between '>
           <div className='flex flex-col items-center xl:w-[50%]'>
             <div
-              className='h-[19rem] xl:h-[24rem] w-[16rem] xl:w-[20rem] p-[1rem] shadow-[0px_9px_43px_0px_#282b6f1a] relative rounded-[1rem]'
+              className='h-[19rem] xl:h-[24rem] w-[16rem] xl:w-[21rem] p-[1rem] xl:p-[1.2rem] bg-[#FFFFFF] shadow-[0px_9px_43px_0px_#282b6f1a] relative rounded-[1rem]'
               style={{ fontFamily: 'poppins, sans-serif' }}
             >
-              <h3 className='h-[2rem] w-[6.5rem] flex justify-center items-center rounded-[2rem] bg-[#CDD8FF] text-[0.8rem] text-[#717FB0]'>
+              <Image
+                className='hidden xl:flex h-[6rem] w-[auto] absolute left-[-2rem] top-[-1.3rem] z-[-3]'
+                src={Purple}
+                alt=''
+              />
+              <Image
+                className='bg-white hidden xl:flex absolute right-[1rem] top-[-1rem] z-[1] h-[3.2rem] w-[3.2rem] shadow-[0px_0px_20px_1px_#edf2f7] rounded-[100%]'
+                src={Close}
+                alt=''
+              />
+              <Image
+                className='hidden xl:flex absolute bg-white right-[-1rem] top-[1.6rem] z-[1] h-[3.2rem] w-[3.2rem] shadow-[0px_0px_20px_1px_#edf2f7] rounded-[100%]'
+                src={Success}
+                alt=''
+              />
+              <h3 className='h-[2rem] w-[6.5rem] flex justify-center items-center rounded-[2rem] bg-[#CDD8FF] text-[0.8rem] text-[#717FB0] xl:text-[1rem] xl:h-[2.3rem] xl:w-[8rem]'>
                 Question 1
               </h3>
-              <p className='text-[1.2rem] text-[#55578D] font-[600] mt-[1rem] xl:mt-[1.5rem]'>
+              <p className='text-[1.2rem] text-[#55578D] font-[600] mt-[1rem] xl:mt-[1.5rem] xl:text-[1.6rem]'>
                 True or false? This play takes place in Italy
               </p>
               <Image
-                className='mt-[1.4rem] z-[1] xl:h-[11rem] xl:w-auto absolute rounded-[1rem]'
+                className='mt-[1.4rem] z-[1] xl:h-[12rem] xl:w-auto absolute rounded-[1rem]'
                 src={River}
                 alt=''
               />
-              <div className='absolute right-[0rem] bottom-[-1rem] h-[4rem] w-[13rem] rounded-[1rem] z-[2] shadow-[0px_18px_52px_0px_#282b6f14] bg-[#FFFFFF] flex items-center px-[1rem] justify-center'>
-                <Image
-                  className='h-[2.4rem] w-auto mr-[1rem]'
-                  src={Plane}
-                  alt=''
-                />
-                <p className='text-[#41BE90] font-[600] text-[0.8rem]'>
-                  Your answer was sent successfully
-                </p>
+              <div className='absolute right-[0rem] bottom-[-1rem] h-[4rem] w-[13rem] rounded-[1rem] z-[2] shadow-[0px_18px_52px_0px_#282b6f14] bg-[#FFFFFF] flex items-center px-[1rem] justify-center hover:scale-[1.05] duration-[0.3s]'>
+                <div className='relative mr-[1rem]'>
+                  <Image className='h-[5.5rem] w-auto' src={Plane} alt='' />
+                  <Image
+                    className='absolute h-[2.5rem] z-[-1] top-[1.6rem] left-[-0.1rem]'
+                    src={GreenTransparent}
+                    alt=''
+                  />
+                </div>
+                <div className='relative'>
+                  <p className='text-[#41BE90] font-[600] text-[0.8rem]'>
+                    Your answer was sent successfully
+                  </p>
+                  <Image
+                    className='absolute h-[1.4rem] z-[-1] bottom-[-0.3rem] right-[-1.6rem]'
+                    src={GreenTransparent}
+                    alt=''
+                  />
+                </div>
               </div>
             </div>
           </div>
