dc353573b678bf086637cc1a8d8b6561d8bb5266
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-18 12:55:31 +0700

    feat: add unit test for text

---
+++ app/components/discussion/discussion.test.tsx
@@ -0,0 +1,17 @@
+import { screen, render } from '@testing-library/react';
+import Discussion from './discussion';
+
+describe('test text & test img alt', () => {
+  test('test text', () => {
+    render(<Discussion />);
+    const h1 = screen.getByRole('heading', { level: 1 });
+    const title = 'One-on-One Discussions';
+    const desc = screen.getByText(
+      'Teachers and teacher assistants can talk with students privately without leaving the Zoom environment.',
+    );
+
+    expect(h1).toBeInTheDocument();
+    expect(h1).toHaveTextContent(title);
+    expect(desc).toBeInTheDocument();
+  });
+});
