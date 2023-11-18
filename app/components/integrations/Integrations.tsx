4e92b2a1d6a6f6bcf1e6b1210ab55f66d65ad213
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-18 17:13:40 +0700

    feat: add unit test for alt img

---
+++ app/components/integrations/Integrations.tsx
@@ -0,0 +1,14 @@
+import Padding from '../padding/padding';
+
+export default function Integrations() {
+  return (
+    <Padding>
+      <div>
+        <div>
+          <div>kkk</div>
+          <div></div>
+        </div>
+      </div>
+    </Padding>
+  );
+}
