87c15501c5abeb8e1d8c244691430e6a46333959
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-18 11:16:35 +0700

    feat: add unit test for text

---
+++ app/components/forEducator/forEducator.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import ForEducator from './forEducator';
+
+describe('test text and img alt', () => {
+  test('test text', () => {
+    render(<ForEducator />);
+    const h1 = screen.getByRole('heading', { level: 1 });
+    const title = 'Class Management Tools for Educators';
+    const desc = screen.getByText(
+      'Class provides tools to help run and manage the class such as Class Roster, Attendance, and more. With the Gradebook, teachers can review and grade tests and quizzes in real-time.',
+    );
+
+    expect(h1).toBeInTheDocument();
+    expect(h1).toHaveTextContent(title);
+    expect(desc).toBeInTheDocument();
+  });
+});
