b5a6133a1471333028114918e3ba7ee0d7a52325
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-18 12:32:11 +0700

    feat: add responsive design for dekstop view

--- app/components/discussion/discussion.tsx
+++ app/components/discussion/discussion.tsx
@@ -9,22 +9,26 @@ export default function Discussion() {
         className='mt-[7rem] flex justify-center'
         style={{ fontFamily: 'poppins, sans-serif' }}
       >
-        <div className='min-[500px]:w-[30rem]'>
-          <div className='flex flex-col items-center'>
+        <div className='min-[500px]:w-[30rem] lg:w-full'>
+          <div className='flex flex-col items-center lg:flex-row lg:justify-between'>
             <div>
-              <Image className='h-[13rem] w-auto' src={DiscussionImg} alt='' />
+              <Image
+                className='h-[13rem] lg:h-[17rem] xl:h-[23rem] w-auto'
+                src={DiscussionImg}
+                alt=''
+              />
             </div>
-            <div className='mt-[1.2rem] lg:mt-0'>
-              <h1 className='text-[#F48C06] font-[600] text-center lg:text-left text-[1.5rem]'>
+            <div className='mt-[1.2rem] lg:mt-0 lg:w-[50%] xl:w-[40%]'>
+              <h1 className='lg:w-[15rem] text-[#F48C06] font-[600] text-center lg:text-[2rem] lg:text-left text-[1.5rem]'>
                 <span className='text-[#2F327D]'>One-on-One</span> Discussions
               </h1>
-              <p className='mt-[1rem] text-center text-[#696984]'>
+              <p className='mt-[1rem] text-center lg:text-[1.2rem] lg:text-justify text-[#696984]'>
                 Teachers and teacher assistants can talk with students privately
                 without leaving the Zoom environment.
               </p>
             </div>
           </div>
-          <div className='flex justify-center mt-[4rem]'>
+          <div className='flex justify-center mt-[7rem]'>
             <button className=' h-[3rem] w-[12rem] rounded-[2rem] flex justify-center items-center border-[1px] border-solid border-[#F48C06] text-[#F48C06] hover:bg-[#F48C06] hover:text-white hover:scale-[1.05] active:scale-[0.99] duration-[0.3s]'>
               See more features
             </button>
