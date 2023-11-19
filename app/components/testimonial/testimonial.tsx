0859ee83ed381196f68b54d17d835df131e3575e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-19 19:10:54 +0700

    feat: add hover effect

--- app/components/testimonial/testimonial.tsx
+++ app/components/testimonial/testimonial.tsx
@@ -27,34 +27,45 @@ export default function Testimonial() {
             <p className='text-[#696984] lg:text-[1.2rem] mt-[1.6rem] text-center lg:text-left'>
               Are you too? Please give your assessment
             </p>
-            <button className=' text-[#F48C06] relative mt-[2rem] h-[3rem] w-[17rem] rounded-[2rem] flex items-center justify-center lg:justify-start border-[1px] border-solid border-[#F48C06] lg:pl-[1.5rem] hover:text-[#fff] hover:bg-[#F48C06] hover:scale-[1.05] active:scale-[0.99] duration-[0.3s]'>
+            <button
+              className={`text-[#F48C06] relative mt-[2rem] xl:mt-[4rem] h-[3rem] w-[17rem] rounded-[2rem] flex items-center justify-center lg:justify-start border-[1px] border-solid  border-[#F48C06] xl:border-r-[0px]  xl:hover:scale-100 xl:active:scale-100 xl:hover:w-[19rem] lg:pl-[1.5rem] hover:text-[#fff] hover:bg-[#F48C06] hover:scale-[1.05] active:scale-[0.99] duration-[0.3s]`}
+            >
               <span className=''>Write your assessment</span>
-              <div className='hidden lg:flex absolute right-0 border-[1px] rounded-[100%] border-solid border-[#F48C06] justify-center h-[100%] w-[3rem] items-center'>
+              <div
+                className={`hidden lg:flex absolute right-0 border-[1px] rounded-[100%] border-solid border-[#F48C06] bg-white justify-center h-[100%] w-[3rem] items-center duration-[0.3s]`}
+              >
                 <Image
                   className='h-[1.2rem] w-auto right-0 '
                   src={ArrowRight}
                   alt=''
                 />
+                {/* 
+                 
+                */}
               </div>
             </button>
           </div>
           <div className='mt-[11rem] lg:mt-[0rem] relative  w-full flex justify-center'>
             <Image
-              className='absolute top-[-7rem] z-[-1] lg:static h-[10rem] lg:h-[23rem] w-auto rounded-[0.5rem]'
+              className='absolute top-[-7rem] z-[-1] lg:static h-[10rem] lg:h-[23rem] xl:h-[28rem] w-auto rounded-[0.5rem]'
               src={SmileGirl}
               alt=''
             />
-            <div className='min-[500px]:w-[20rem] lg:absolute top-[8rem] bg-[#FFFFFF] shadow-[0_3px_10px_rgb(0,0,0,0.2)] py-[1.2rem] px-[1rem] rounded-[1rem]'>
-              <div>
+            <div className='min-[500px]:w-[20rem] xl:w-[28rem] lg:absolute top-[18rem] right-[3rem] bg-[#FFFFFF] shadow-[0_3px_10px_rgb(0,0,0,0.2)] py-[1.2rem] px-[1rem] xl:px-[1.5rem] xl:border-l-[8px] border-solid border-[#F67766] rounded-[1rem]'>
+              <div className='xl:pl-[1rem]  border-l-[1px] border-solid border-[#BDBDD1]'>
                 <p className='text-[#5F5F7E] font-[400]'>
                   {`"Thank you so much for your help. It's exactly what I've been looking for. You won't regret it. It really saves me time and effort. Skilline is exactly what our business has been lacking."`}
                 </p>
               </div>
-              <div className='mt-[2rem] flex items-center justify-between'>
+              <div className='mt-[2rem] flex items-center justify-between xl:pl-[1rem]'>
                 <h2 className='text-[#5F5F7E] font-[600]'>Gloria Rose</h2>
                 <div className='flex flex-col items-end'>
-                  <Image className='h-auto w-[5rem]' src={StarGroup} alt='' />
-                  <p className='mt-[0.3rem] text-[0.7rem] text-[#80819A] font-[600]'>
+                  <Image
+                    className='h-auto w-[5rem] xl:w-[6rem]'
+                    src={StarGroup}
+                    alt=''
+                  />
+                  <p className='mt-[0.3rem] xl:mt-[0.5rem] text-[0.7rem] xl:text-[0.8rem] text-[#80819A] font-[600]'>
                     12 reviews at Yelp
                   </p>
                 </div>
