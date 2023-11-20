9394934e8fc107c0337a1dba6c39e9e7690f8a77
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-20 11:03:11 +0700

    feat: add mock data

---
+++ app/components/lastestNews/newsData.ts
@@ -0,0 +1,39 @@
+import TeamsMeet from '@/assets/lastestNews/teams-meet.png';
+import KidLearning from '@/assets/lastestNews/kid-learning.png';
+import MeetWithTeams from '@/assets/lastestNews/meet-with-team.png';
+import MeetWithCat from '@/assets/lastestNews/meet-with-cat.png';
+
+export const bigNews = [
+  {
+    img: TeamsMeet,
+    label: 'NEWS',
+    title:
+      'Class adds $30 million to its balance sheet for a Zoom-friendly edtech solution',
+    desc: 'Class, launched less than a year ago by Blackboard co-founder Michael Chasen, integrates exclusively...',
+    btnText: 'Read more',
+  },
+];
+
+export const smallNews = [
+  {
+    img: KidLearning,
+    label: 'PRESS RELEASE',
+    title:
+      'Class Technologies Inc. Closes $30 Million Series A Financing to Meet High Demand',
+    desc: 'Class Technologies Inc., the company that created Class,...',
+  },
+  {
+    img: MeetWithTeams,
+    label: 'NEWS',
+    title:
+      'Zoom’s earliest investors are betting millions on a better Zoom for schools',
+    desc: 'Zoom was never created to be a consumer product. Nonetheless, the...',
+  },
+  {
+    img: MeetWithCat,
+    label: 'NEWS',
+    title:
+      'Former Blackboard CEO Raises $16M to Bring LMS Features to Zoom Classrooms',
+    desc: 'This year, investors have reaped big financial returns from betting on Zoom...',
+  },
+];
