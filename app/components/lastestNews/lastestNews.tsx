d8fc8e9861fe404ed014809b73b3f606f5a7f87b
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-20 10:51:56 +0700

    feat: styling h1 & p element

--- app/components/lastestNews/lastestNews.tsx
+++ app/components/lastestNews/lastestNews.tsx
@@ -5,9 +5,14 @@ export default function LastestNews() {
     <Padding>
       <div>
         <div>
-          <div>
-            <h1>Frontend Web Developer Intern</h1>
-            <p>
+          <div
+            className='flex flex-col items-center'
+            style={{ fontFamily: 'nunito, sans-serif' }}
+          >
+            <h1 className='w-[18rem] text-center text-[#2F327D] text-[1.5rem] font-[600]'>
+              Frontend Web Developer Intern
+            </h1>
+            <p className='mt-[1rem] text-[#696984] text-center'>
               See the developments that have occurred to Skillines in the world
             </p>
           </div>
