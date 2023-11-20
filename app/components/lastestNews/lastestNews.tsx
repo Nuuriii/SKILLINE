45b4d58225370e11ab87e023bb5fb293a2fa829e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-11-20 12:59:12 +0700

    feat: add responsive design for tablet view

--- app/components/lastestNews/lastestNews.tsx
+++ app/components/lastestNews/lastestNews.tsx
@@ -6,21 +6,21 @@ import { bigNews, smallNews } from './newsData';
 export default function LastestNews() {
   return (
     <Padding>
-      <div className='flex justify-center'>
-        <div className='min-[500px]:w-[30rem]'>
+      <div className='lg:mt-[11rem] flex justify-center'>
+        <div className='min-[500px]:w-[30rem] lg:w-full'>
           <div
             className='flex flex-col items-center'
             style={{ fontFamily: 'nunito, sans-serif' }}
           >
-            <h1 className='w-[18rem] text-center text-[#2F327D] text-[1.5rem] font-[600]'>
+            <h1 className='w-[18rem] lg:w-auto text-center text-[#2F327D] text-[1.5rem] lg:text-[2rem] font-[600]'>
               Frontend Web Developer Intern
             </h1>
-            <p className='mt-[1rem] text-[#696984] text-center'>
+            <p className='mt-[1rem] lg:text-[1.2rem] text-[#696984] text-center'>
               See the developments that have occurred to Skillines in the world
             </p>
           </div>
-          <div className='flex flex-col'>
-            <div>
+          <div className='lg:mt-[3rem] flex flex-col lg:flex-row'>
+            <div className='lg:mr-[4rem]'>
               {bigNews.map((item, index) => (
                 <Card
                   key={index}
@@ -37,7 +37,7 @@ export default function LastestNews() {
                 <div key={index} className='flex mb-[1.5rem]'>
                   <div className='w-[40%] mr-[1rem] relative'>
                     <Image className='rounded-[1rem]' src={item.img} alt='' />
-                    <p className='hidden lg:flex text-[0.4rem] absolute bg-[#F4C467] text-[#252641]'>
+                    <p className='hidden absolute bottom-[0.7rem] right-[0.7rem] py-[0.2rem] px-[0.7rem] rounded-[2rem] lg:flex text-[0.7rem] bg-[#F4C467] text-[#252641]'>
                       {item.label}
                     </p>
                   </div>
