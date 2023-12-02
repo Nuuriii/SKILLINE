eb7d76de3ec4a72ab590a73536bc093ddd89efbd
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-02 10:06:54 +0700

    refactor: combine tailwind css with css module

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -9,83 +9,73 @@ import Mail from '@/assets/header/email.svg';
 import Statistic from '@/assets/header/statistic.png';
 import GreenBullet from '@/assets/header/greenbullet.svg';
 import CheckIcon from '@/assets/header/checklist.svg';
-import './header.css';
+import styles from './header.module.css';
 
 export default function Header() {
   return (
-    <div className='relative flex justify-center'>
-      <Image
-        className='h-[95vh] hidden lg:flex min-[1488px]:h-[40rem] min-[1488px]:w-[100%] absolute z-[-2]'
-        src={Bg}
-        alt='orangeBg'
-      />
-      <div className='w-full bg-[#FFF2E1] lg:bg-transparent min-[1488px]:w-[70rem]'>
+    <div className={styles.mainContainer}>
+      <Image className={styles.orangeBg} src={Bg} alt='orangeBg' />
+      <div className={styles.childContainer}>
         <Padding>
-          <div className='flex justify-center'>
-            <div className='flex pt-[4rem] lg:pt-0 flex-col items-center justify-center lg:flex-row lg:items-center lg:justify-between  h-[100vh] md:h-[42rem] lg:h-[100vh] min-[1488px]:h-[45rem] min-[500px]:w-[30rem] lg:w-full'>
-              <div className='flex lg:hidden'>
-                <Image
-                  className='shadow-[ 0px 4px 100px 0px rgba(29, 28, 24, 0.25)] h-[13rem] w-auto md:h-[18rem]'
-                  src={Girl}
-                  alt='girlMobile'
-                />
+          <div className={styles.viewContainer}>
+            <div className={styles.contentContainer}>
+              <div className={styles.girlImgContainer}>
+                <Image className={styles.girlImg} src={Girl} alt='girlMobile' />
               </div>
-              <div className='w-[100%] mt-[3rem] md:mt-[0]  lg:w-[50%]'>
-                <h1 className='text-[#2F327D] font-[700] text-center text-[2rem] lg:text-start lg:text-[2.9rem] '>
-                  <span className='text-[#F48C06]'>Studying</span> Online is now
-                  much easier
+              <div className={styles.descriptionContainer}>
+                <h1 className={styles.titleDescription}>
+                  <span className={styles.spanTitleText}>Studying</span> Online
+                  is now much easier
                 </h1>
-                <p className='text-[#464646] lg:w-[80%] my-[1rem] md:my-[2rem] text-center lg:text-start text-[1.1rem] md:text-[1.3rem]'>
+                <p className={styles.descriptionParagraph}>
                   Skilline is an interesting platform that will teach you in
                   more an interactive way
                 </p>
-                <div className='flex items-center justify-center lg:justify-start'>
-                  <button className='h-[3rem] w-[9rem] font-[600] rounded-[2rem] bg-[#F48C06] text-[#FFFFFF] flex justify-center items-center hover:scale-[1.05] duration-[0.3s] active:scale-[0.98]'>
+                <div className={styles.buttonsContainer}>
+                  <button className={styles.firstBtnItems}>
                     Join for free
                   </button>
-                  <button className='hover:scale-[1.05] duration-[0.3s] active:scale-[0.98]'>
+                  <button className={styles.secondBtnItems}>
                     <Image
-                      className='h-[3.7rem] w-auto mx-[1.5rem]'
+                      className={styles.playIcon}
                       src={Play}
                       alt='playIcon'
                     />
                   </button>
-                  <p className='text-[#252641] hidden lg:flex'>
-                    Watch how it works
-                  </p>
+                  <p className={styles.paragraphItems}>Watch how it works</p>
                 </div>
               </div>
-              <div className='mr-[4rem] xl:mr-[10rem] min-[1488px]:mr-[2rem] relative hidden lg:block '>
-                <div className='absolute hidden xl:flex right-[-2rem] top-[3rem]'>
+              <div className={styles.imageContainer}>
+                <div className={styles.statisticContainer}>
                   <Image
-                    className='h-[3rem] w-auto'
+                    className={styles.statisticIcon}
                     src={Statistic}
                     alt='statisticIcon'
                   />
                 </div>
 
-                <div className='absolute right-[-8rem] top-[11rem] hidden xl:flex py-[0.7rem] px-[1rem] backdrop-blur-[7px] rounded-[1rem] bg-[#FFFFFFCC] items-center'>
-                  <div className='bg-[#F88C3D] h-[2.5rem] w-[auto] rounded-[0.3rem] flex justify-center items-center mr-[1rem] p-[0.3rem]'>
+                <div className={styles.mailContainer}>
+                  <div className={styles.mailIconContainer}>
                     <Image
-                      className='h-[100%] w-[100%]'
+                      className={styles.mailIcon}
                       src={Mail}
                       alt='mailIcon'
                     />
                   </div>
-                  <div className='relative'>
+                  <div className={styles.mailInfoContainer}>
                     <Image
-                      className='absolute right-0'
+                      className={styles.checkIcon}
                       src={CheckIcon}
                       alt='checkIcon'
                     />
                     <h2
-                      className='text-[#595959] font-[700]'
+                      className={styles.mailInfoTitle}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       Congratulations
                     </h2>
                     <p
-                      className='text-[#545567] text-[1rem]'
+                      className={styles.mailInfoParagraph}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       Your admission completed
@@ -93,54 +83,50 @@ export default function Header() {
                   </div>
                 </div>
 
-                <div className='absolute left-[-8rem] top-[4rem] hidden xl:flex py-[0.7rem] px-[1rem] backdrop-blur-[5px] rounded-[1rem] bg-[#FFFFFFCC] items-center'>
-                  <div className='bg-[#23BDEE] h-[2.5rem] w-[auto] rounded-[0.3rem] flex justify-center items-center mr-[1rem] p-[0.3rem]'>
+                <div className={styles.calendarContainer}>
+                  <div className={styles.calendarIconContainer}>
                     <Image
-                      className='h-[100%] w-[100%]'
+                      className={styles.calendarIcon}
                       src={Calender}
                       alt='calenderIcon'
                     />
                   </div>
                   <div>
                     <h2
-                      className='text-[#595959] font-[700]'
+                      className={styles.calendarTitle}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       250k
                     </h2>
                     <p
-                      className='text-[#545567] text-[1rem]'
+                      className={styles.calendarParagraph}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       Assisted Student
                     </p>
                   </div>
                 </div>
-                <div className='absolute bottom-[5rem] left-[-11rem] backdrop-blur-[5px] bg-[#FFFFFFCC] hidden xl:flex py-[1rem] px-[1rem] rounded-[1rem]'>
-                  <div className='mr-[1rem] relative'>
+                <div className={styles.userExperienceContainer}>
+                  <div className={styles.userImageContainer}>
+                    <Image className={styles.userImg} src={Man} alt='manIcon' />
                     <Image
-                      className='h-[2.4rem] w-auto'
-                      src={Man}
-                      alt='manIcon'
-                    />
-                    <Image
-                      className='absolute h-[0.9rem] w-auto top-[1.5rem] right-[-0.2rem] border-[1.5px] border-solid border-[#FBECD7] rounded-[100%]'
+                      className={styles.greenBulletIcon}
                       src={GreenBullet}
                       alt='bulletIcon'
                     />
                   </div>
                   <div>
                     <h2
-                      className='text-[#595959] font-[700]'
+                      className={styles.experienceTitle}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       User Experience Class
                     </h2>
-                    <p className='text-[#545567] mb-[1rem] text-[0.9rem]'>
+                    <p className={styles.experienceParagraph}>
                       Today at 12.00 PM
                     </p>
                     <button
-                      className='h-[2.5rem] w-[8rem] rounded-[2rem] font-[700] flex justify-center items-center bg-[#D8587E] text-[#FFFFFF] hover:scale-[1.05] duration-[0.3s] active:scale-[0.98]'
+                      className={styles.experienceBtn}
                       style={{ fontFamily: 'nunito, sans-serif' }}
                     >
                       Join Now
@@ -148,7 +134,7 @@ export default function Header() {
                   </div>
                 </div>
                 <Image
-                  className='h-[30rem] top-[3rem]  w-auto'
+                  className={styles.girlDekstopImg}
                   src={Girl}
                   alt='girlDekstop'
                 />
