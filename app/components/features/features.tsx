27e3d9456fc970d8fc69164a81d996e81f28ee22
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-04 06:17:30 +0700

    refactor: combine tailwind with css module

--- app/components/features/features.tsx
+++ app/components/features/features.tsx
@@ -35,33 +35,22 @@ export default function Features() {
               <Image className={styles.purpleBall} src={PurpleBall} alt='' />
             </div>
 
-            <div className='mt-[4.4rem] lg:mt-[0rem] lg:w-[40%]'>
-              <h2
-                className='text-[#2F327D] hidden lg:block text-center lg:text-left text-[1.3rem] font-[600] lg:text-[1.5rem] xl:w-[30rem]'
-                style={{ fontFamily: 'poppins, sans-serif' }}
-              >
-                A <span className='text-[#F48C06]'>user interface </span>
+            <div className={styles.featureWrapper}>
+              <h2 className={styles.featureTitle}>
+                A <span className={styles.spanText}>user interface </span>
                 designed for the classroom
               </h2>
-              <div className='mt-[3rem] lg:mt-[1rem]'>
+              <div className={styles.featureItemsContainer}>
                 {featureData.map((item, index) => (
-                  <div
-                    className='relative flex items-center rounded-[1rem] px-[0.5rem] pt-[2.5rem] pb-[1rem] lg:shadow-none shadow-[0px_10px_60px_0px_#e2e8f0] mb-[3rem] lg:mb-[1.5rem] lg:p-0 lg:justify-between'
-                    key={index}
-                  >
-                    <div className='absolute lg:static rounded-[100%] h-[3.5rem] top-[-1.7rem] left-[40%] w-[3.5rem] bg-[#FBFBFB] flex justify-center items-center shadow-[0px_14px_44px_0px__#0d0f1c1f] lg:h-[3rem] lg:w-[3rem]'>
+                  <div className={styles.featureItemsWrapper} key={index}>
+                    <div className={styles.featureIconContainer}>
                       <Image
-                        className='h-[1.8rem] w-[1.8rem] lg:h-[1.4rem] lg:w-[1.4rem]'
+                        className={styles.featureIcon}
                         src={item.icon}
                         alt={item.alt}
                       />
                     </div>
-                    <p
-                      className='lg:w-[85%] lg:text-[1.1rem] text-[#696984] text-center lg:text-left'
-                      style={{ fontFamily: 'poppins, sans-serif' }}
-                    >
-                      {item.decs}
-                    </p>
+                    <p className={styles.featureParagraph}>{item.decs}</p>
                   </div>
                 ))}
               </div>
