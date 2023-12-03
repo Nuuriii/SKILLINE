ad6eea61b9a14f04854d5930f4b53c179bdc1b27
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-03 12:54:21 +0700

    refactor: move slider style to css file

--- app/components/company/company.tsx
+++ app/components/company/company.tsx
@@ -9,14 +9,11 @@ export default function Company() {
       <div className={styles.companyContainer}>
         <div className={styles.childContainer}>
           <div>
-            <h1
-              className='text-[1.2rem] md:text-[1.6rem] text-[#696984s] text-center font-[500]'
-              style={{ fontFamily: 'poppins, sans-serif' }}
-            >
+            <h1 className={styles.companyTitle}>
               Trusted by 5,000+ Companies Worldwide
             </h1>
           </div>
-          <div className="relative mt-[4rem] w-[100%] overflow-hidden bg-white before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,white_0%,rgba(255,255,255,0)_100%)] after:content-['']">
+          <div className={styles.sliderContainer}>
             <div className='animate-infinite-slider flex items-center w-[calc(250px*10)]'>
               {CompanyIcon.map((item, index) => (
                 <div
