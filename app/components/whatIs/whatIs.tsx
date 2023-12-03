17b679acc987323bb786684c2362476d83f6958b
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-03 22:30:53 +0700

    refactor: use css module in css

--- app/components/whatIs/whatIs.tsx
+++ app/components/whatIs/whatIs.tsx
@@ -8,17 +8,11 @@ export default function WhatIs() {
     <Padding>
       <div className={styles.mainContainer}>
         <div className={styles.childContainer}>
-          <div className='mb-[2rem] flex flex-col items-center'>
-            <h1
-              className='text-center text-[#2F327D] font-[600] text-[1.5rem] lg:text-[2rem]'
-              style={{ fontFamily: 'poppins, sans-serif' }}
-            >
-              What is <span className='text-[#F48C06]'>Skilline?</span>
+          <div className={styles.descriptionContainer}>
+            <h1 className={styles.titleDescription}>
+              What is <span className={styles.spanText}>Skilline?</span>
             </h1>
-            <p
-              className='mt-[1rem] text-center text-[#696984] lg:text-[1.2rem] lg:w-[55rem]'
-              style={{ fontFamily: 'poppins, sans-serif' }}
-            >
+            <p className={styles.paragraphDescription}>
               Skilline is a platform that allows educators to create online
               classes whereby they can store the course materials online; manage
               assignments, quizzes and exams; monitor due dates; grade results
