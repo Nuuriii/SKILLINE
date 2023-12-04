767337811c3278dd2ed4fe4f7c52c160c46ffaaa
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-04 10:23:33 +0700

    refactor: move style to css module file

--- app/components/forTeachAndLearn/forTeachAndLearn.tsx
+++ app/components/forTeachAndLearn/forTeachAndLearn.tsx
@@ -8,18 +8,12 @@ export default function ForTeachAndLearn() {
     <Padding>
       <div className={styles.mainContainer}>
         <div className={styles.childContainer}>
-          <div className='flex flex-col items-center lg:items-start lg:mr-[6rem] lg:w-[50%]'>
-            <h1
-              className='text-[#2F327D] font-[600] text-center lg:text-left text-[1.5rem] lg:text-[2rem] w-[16rem] lg:w-[20rem]'
-              style={{ fontFamily: 'poppins, sans-serif' }}
-            >
-              <span className='text-[#F48C06]'>Tools</span> For Teachers And
+          <div className={styles.descriptionContainer}>
+            <h1 className={styles.titleDescription}>
+              <span className={styles.spanText}>Tools</span> For Teachers And
               Learners
             </h1>
-            <p
-              className='text-[#696984] text-center lg:text-justify lg:text-[1.2rem] mt-[1rem]'
-              style={{ fontFamily: 'poppins,sans-serif' }}
-            >
+            <p className={styles.paragraphDescription}>
               Class has a dynamic set of teaching tools built to be deployed and
               used during class. Teachers can handout assignments in real-time
               for students to complete and submit.
