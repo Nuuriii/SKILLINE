c9e17902e7ff1a3593c8cc1b71e3f8e8cad31225
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-04 21:06:16 +0700

    refactor: combine tailwind css with css module

--- app/components/forEducator/forEducator.tsx
+++ app/components/forEducator/forEducator.tsx
@@ -8,12 +8,12 @@ export default function ForEducator() {
     <Padding>
       <div className={styles.mainContainer}>
         <div className={styles.childContainer}>
-          <div className='lg:w-[50%] xl:w-[45%] lg:mr-[4rem]'>
-            <h1 className='lg:w-[20rem] text-[#2F327D] font-[600] text-[1.5rem] lg:text-[2rem] text-center lg:text-left'>
-              <span className='text-[#F48C06]'>Class Management</span> Tools for
-              Educators
+          <div className={styles.descriptionContainer}>
+            <h1 className={styles.titleDescription}>
+              <span className={styles.spanText}>Class Management</span> Tools
+              for Educators
             </h1>
-            <p className='mt-[1rem] text-[#696984] lg:text-[1.2rem] text-center lg:text-justify'>
+            <p className={styles.paragraphDescription}>
               Class provides tools to help run and manage the class such as
               Class Roster, Attendance, and more. With the Gradebook, teachers
               can review and grade tests and quizzes in real-time.
