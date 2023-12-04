48669963d0bdec52c4b99c5bc41c1c75fddc7609
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-04 10:07:56 +0700

    refactor: move container style to css module file

--- app/components/forTeachAndLearn/forTeachAndLearn.tsx
+++ app/components/forTeachAndLearn/forTeachAndLearn.tsx
@@ -1,12 +1,13 @@
 import Padding from '../padding/padding';
 import Student from '@/assets/forTeachAndLearn/student.png';
 import Image from 'next/image';
+import styles from './forTeachAndLearn.module.css';
 
 export default function ForTeachAndLearn() {
   return (
     <Padding>
-      <div className='mt-[1rem] lg:mt-[8rem] flex justify-center'>
-        <div className='flex flex-col lg:flex-row items-center min-[500px]:w-[30rem] lg:w-[100%] min-[1488px]:w-[70rem]'>
+      <div className={styles.mainContainer}>
+        <div className={styles.childContainer}>
           <div className='flex flex-col items-center lg:items-start lg:mr-[6rem] lg:w-[50%]'>
             <h1
               className='text-[#2F327D] font-[600] text-center lg:text-left text-[1.5rem] lg:text-[2rem] w-[16rem] lg:w-[20rem]'
