0a52d6634be229d575782cfb511dc7de2a8b24ae
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-06 14:51:31 +0700

    refactor: combine tailwind css with css module

--- app/components/integrations/integrations.tsx
+++ app/components/integrations/integrations.tsx
@@ -25,23 +25,21 @@ export default function Integrations() {
             <Image className={styles.driveIcon} src={Drive} alt='drive-icon' />
             <Image className={styles.teamsIcon} src={Teams} alt='teams-icon' />
           </div>
-          <div className='mt-[3rem] flex flex-col items-center lg:w-[50%] lg:items-start'>
+          <div className={styles.descriptionContainer}>
             <div className={styles.integrationsContainer}>
               <span className={styles.spanBorder}></span>
               <h3 className={styles.integrationsText}>INTEGRATIONS</h3>
             </div>
-            <h1 className='lg:mt-[1rem] text-[#525596] text-[1.5rem] lg:text-[2rem] font-[600] text-center lg:text-left'>
+            <h1 className={styles.titleDescription}>
               200+ educational tools and platform{' '}
-              <span className='text-[#F48C06]'>integrations</span>
+              <span className={styles.spanText}>integrations</span>
             </h1>
-            <p className='text-[#696984] text-center xl:text-[1.2rem] lg:text-left mt-[1rem]'>
+            <p className={styles.paragraphDescription}>
               Schoology has every tool your classroom needs and comes
               pre-integrated with more than 200+ tools, student information
               systems (SIS), and education platforms.
             </p>
-            <button className='mt-[2rem] rounded-[2rem] py-[0.7rem] px-[1.5rem] text-[#F48C06] border-[1px] border-solid border-[#F48C06] font-[400] hover:bg-[#F48C06] hover:text-white hover:scale-[1.05] active:scale-[0.99] duration-[0.3s]'>
-              See All Integrations
-            </button>
+            <button className={styles.seeAllBtn}>See All Integrations</button>
           </div>
         </div>
       </div>
