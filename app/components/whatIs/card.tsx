5341a280fa3f79a4a7999bd872d628b498962735
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-17 08:31:22 +0700

    feat: add fade transition

--- app/components/whatIs/card.tsx
+++ app/components/whatIs/card.tsx
@@ -1,5 +1,8 @@
 import Image, { StaticImageData } from 'next/image';
 import styles from './whatIs.module.css';
+import { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 interface CardProps {
   index: number;
@@ -9,8 +12,18 @@ interface CardProps {
 }
 
 export default function Card({ index, bgImg, title, btnText }: CardProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      offset: 0,
+    });
+  }, []);
+
   return (
-    <div className={styles.cardWrapper}>
+    <div
+      className={styles.cardWrapper}
+      data-aos={`${index == 1 ? 'fade-left' : 'fade-right'}`}
+    >
       <div className={styles.imgContainer}>
         <Image className={styles.bgImgStyles} src={bgImg} alt='' />
       </div>
